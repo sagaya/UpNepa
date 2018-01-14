a50664404700bcfca156dd69530ec2c8d2bfc19e
Sagaya Abdulhafeez <sagayaabdulhafeez@macs-MacBook-Pro.local>
2018-01-14 15:02:59 +0100

    Increased Token expiration

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from os import environ
+import datetime
 
 
 class Base:
@@ -6,4 +7,5 @@ class Base:
     JWT_SECRET_KEY = 'OPTISS'
     SECRET_KEY = 'OPTISS'
     DB = environ.get('DB')
+    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(1440000)
     
