75403f64c0a53e20c130e134e10c56ddb74253b2
Sagaya Abdulhafeez <sagayaabdulhafeez@macs-MacBook-Pro.local>
2018-01-14 15:27:43 +0100

    sda

--- resources.py
+++ resources.py
@@ -42,7 +42,7 @@ class UpdateUser(Resource):
         if user is not None:
             if user.chatId is not None:
                 if data["state"] is True:
-                    time = datetime.datetime.now().strftime("%b %d, %-M:%H %p")
+                    time = datetime.datetime.now().strftime("%b %d, %m:%H %p")
                     bot.sendMessage(
                         user.chatId, "Them don bring light!. The time wey them bring am na {}".format(time))
                     return {"status": True}
