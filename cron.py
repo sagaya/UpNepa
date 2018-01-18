fe7efa5eb338e89c2777a0646982ebbb45d0ba0e
Sagaya Abdulhafeez <sagayaabdulhafeez@macs-MacBook-Pro.local>
2018-01-18 01:31:33 +0100

    MessageLoop

--- cron.py
+++ cron.py
@@ -1,42 +1,24 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-import telepot
-import json
 from models.user import User
 from os import environ
+offset = None
+import telepot
+from telepot.loop import MessageLoop
+import pprint
 
 bot = telepot.Bot('{}'.format(environ.get("TOKEN")))
 
-offset = None
+def handle(jsonObject):
+    username = jsonObject["from"]["username"]
+    user = User.objects(username=username).first()
+    if user:
+        user.chatId = jsonObject["from"]["id"]
+        user.save()
+        if jsonObject["text"] == "/start":
+            bot.sendMessage(
+                jsonObject["from"]["id"], "Hi {}, Welcome to UpNepa. UpNepa is a bot that helps you keep track of PHCN power supply.".format(user.username))
+            bot.sendMessage(
+                jsonObject["from"]["id"], "Congratulations {}! You can now receive notifications for power satus via telegram.".format(user.username))
+    else:
+        bot.sendMessage(jsonObject["from"]["id"], "Your username is not recognized please set a valid username!")
 
-def new_stuff():
-    global offset
-    print(offset)
-    content = json.dumps(bot.getUpdates(offset=offset))
-    jsonObjects = json.loads(content)
-    updates = [int(x["update_id"]) for x in jsonObjects]
-    offset = max(updates, default=0) + 1
-    print(jsonObjects)
-    for jsonObject in jsonObjects:
-        if offset != jsonObject["update_id"]:
-            if jsonObject:
-                print(jsonObject)
-                username = jsonObject["message"]["from"]["username"]
-                user = User.objects(username=username).first()
-                if user:
-                    user.chatId = jsonObject["message"]["from"]["id"]
-                    user.save()
-                    if jsonObject["message"]["text"] == "/start":
-                        bot.sendMessage(jsonObject["message"]["from"]["id"], "Hi {}, Welcome to UpNepa. UpNepa is a bot that helps you keep track of PHCN power supply.".format(user.username))
-                        bot.sendMessage(jsonObject["message"]["from"]["id"], "Congratulations {}! You can now receive notifications for power satus via telegram.".format(user.username))
-                        break
-                else:
-                     bot.sendMessage(
-                         jsonObject["message"]["from"]["id"], "Your username is not recognized please set a valid username!")
-
-    #Check for new messages and assign the new stuff to the user
-
-sched = BackgroundScheduler()
-
-sched.add_job(new_stuff, 'interval', seconds=5)
-
-sched.start()
+MessageLoop(bot, handle).run_as_thread()
