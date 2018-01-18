89487bbcf3096fde4af3de00b042f429f1d9d71e
Sagaya Abdulhafeez <sagayaabdulhafeez@macs-MacBook-Pro.local>
2018-01-18 07:00:12 +0100

    cron

--- cron.py
+++ cron.py
@@ -18,6 +18,9 @@ def handle(jsonObject):
                 jsonObject["from"]["id"], "Hi {}, Welcome to UpNepa. UpNepa is a bot that helps you keep track of PHCN power supply.".format(user.username))
             bot.sendMessage(
                 jsonObject["from"]["id"], "Congratulations {}! You can now receive notifications for power satus via telegram.".format(user.username))
+        else:
+            bot.sendMessage(
+                jsonObject["from"]["id"], "Invalid Command")
     else:
         bot.sendMessage(jsonObject["from"]["id"], "Your username is not recognized please set a valid username!")
 
