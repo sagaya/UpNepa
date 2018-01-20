fa8f7276fd6fc9d1deff9b833f6a208699e1004d
Sagaya Abdulhafeez <sagayaabdulhafeez@macs-MacBook-Pro.local>
2018-01-20 09:12:45 +0100

    cron

--- resources.py
+++ resources.py
@@ -10,8 +10,34 @@ import datetime
 from os import environ
 from flask_wtf import FlaskForm
 from wtforms import StringField
+from os import environ
+offset = None
+from telepot.loop import MessageLoop
+
+
+def handle(jsonObject):
+    username = jsonObject["from"]["username"]
+    user = User.objects(username=username).first()
+    if user:
+        user.chatId = jsonObject["from"]["id"]
+        user.save()
+        if jsonObject["text"] == "/start":
+            bot.sendMessage(
+                jsonObject["from"]["id"], "Hi {}, Welcome to UpNepa. UpNepa is a bot that helps you keep track of PHCN power supply.".format(user.username))
+            bot.sendMessage(
+                jsonObject["from"]["id"], "Congratulations {}! You can now receive notifications for power satus via telegram.".format(user.username))
+        else:
+            bot.sendMessage(
+                jsonObject["from"]["id"], "Invalid Command")
+    else:
+        bot.sendMessage(
+            jsonObject["from"]["id"], "Your username is not recognized please set a valid username!")
+
 
 bot = telepot.Bot('{}'.format(environ.get("TOKEN")))
+bot.message_loop(handle)
+bot.setWebhook()
+
 
 class Index(Resource):
 
