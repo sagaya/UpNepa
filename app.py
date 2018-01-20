c070e475963da4b6b86bd3555e683d9d11aa4c8a
Sagaya Abdulhafeez <sagayaabdulhafeez@macs-MacBook-Pro.local>
2018-01-20 09:49:26 +0100

    WDVs

--- app.py
+++ app.py
@@ -47,39 +47,6 @@ def index():
     return render_template("index.html", form=form)
 
 
-from models.user import User
-from os import environ
-offset = None
-import telepot
-from telepot.loop import MessageLoop
-import pprint
-import time
-
-
-def handle(jsonObject):
-    username = jsonObject["from"]["username"]
-    user = User.objects(username=username).first()
-    if user:
-        user.chatId = jsonObject["from"]["id"]
-        user.save()
-        if jsonObject["text"] == "/start":
-            bot.sendMessage(
-                jsonObject["from"]["id"], "Hi {}, Welcome to UpNepa. UpNepa is a bot that helps you keep track of PHCN power supply.".format(user.username))
-            bot.sendMessage(
-                jsonObject["from"]["id"], "Congratulations {}! You can now receive notifications for power satus via telegram.".format(user.username))
-        else:
-            bot.sendMessage(
-                jsonObject["from"]["id"], "Invalid Command")
-    else:
-        bot.sendMessage(
-            jsonObject["from"]["id"], "Your username is not recognized please set a valid username!")
-
-
-bot = telepot.Bot('{}'.format(environ.get("TOKEN")))
-bot.setWebhook()
-bot.message_loop(handle)
-
-
 if __name__ == '__main__':
     # bot.setWebhook()
     app.run()
