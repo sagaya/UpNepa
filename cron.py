ad1aa9687fcc7ff4b550268c733a59e8a454d20d
Sagaya Abdulhafeez <sagayaabdulhafeez@macs-MacBook-Pro.local>
2018-01-21 15:43:35 +0100

    V1

--- cron.py
+++ cron.py
@@ -3,11 +3,8 @@ offset = None
 import telepot
 from telepot.loop import MessageLoop
 import time
-
 from apscheduler.schedulers.blocking import BlockingScheduler
-import telepot
 import json
-from models.user import User
 from os import environ
 from mongoengine import *
 from config import Base
@@ -38,63 +35,6 @@ def fetch():
     print("Howdy")
     MessageLoop(bot, handle).run_as_thread()
     print('Listening ...')
-
-
-# def get_offset():
-#     global initial
-#     content = json.dumps(bot.getUpdates())
-#     jsonObjects = json.loads(content)
-#     print(jsonObjects)
-#     updates = [int(x["update_id"]) for x in jsonObjects]
-#     offset = max(updates, default=0) 
-#     if offset != 0:
-#         initial = False
-#     return offset
-
-
-# initial = False
-
-
-def new_stuff():
-    print("Howdy")
-    global initial
-    off = get_offset()
-    offset = 0
-    if initial is False:
-        offset = off
-    else:
-        offset = off + 1
-
-    print("Offset is {}".format(offset))
-    content = json.dumps(bot.getUpdates(offset=offset))
-    jsonObjects = json.loads(content)
-    print(jsonObjects)
-    initial = True
-    print(initial)
-
-    for jsonObject in jsonObjects:
-        print("SADdsads")
-        if offset != jsonObject["update_id"] or offset is not None:
-            if jsonObject:
-                print(jsonObject)
-                username = jsonObject["message"]["from"]["username"]
-                user = User.objects(username=username).first()
-                if user:
-                    user.chatId = jsonObject["message"]["from"]["id"]
-                    user.save()
-                    if jsonObject["message"]["text"] == "/start":
-                        bot.sendMessage(
-                            jsonObject["message"]["from"]["id"], "Hi {}, Welcome to UpNepa. UpNepa is a bot that helps you keep track of PHCN power supply.".format(user.username))
-                        offset = jsonObject["update_id"] + 1
-                        bot.sendMessage(
-                            jsonObject["message"]["from"]["id"], "Congratulations {}! You can now receive notifications for power satus via telegram.".format(user.username))
-                        break
-                    bot.sendMessage(
-                        jsonObject["message"]["from"]["id"], "{} is an Invalid Command".format(jsonObject["message"]["text"]))
-                else:
-                     bot.sendMessage(
-                         jsonObject["message"]["from"]["id"], "Your username is not recognized please set a valid username!")
-
 
 sched = BlockingScheduler()
 
